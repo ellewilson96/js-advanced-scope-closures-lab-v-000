ac18a0c19151b4d1c8c3798d79d55b4863251dae
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-28 00:50:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
+    const startBlock = parseInt(start)
+    const endBlock = parseInt(end)
+    const distance = startBlock > endBlock ? (startBlock - endBlock) : (endBlock - startBlock)
+    if (distance <= blockRange) {
+      return `within range by ${blockRange - distance}`;
+    } else {
+      return `${distance - blockRange} blocks out of range`;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
 function createDriver() {
   let driverId = 0;
 
